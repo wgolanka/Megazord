3473379f920579b902b65eed1a9de95c8e7e8bad
wgolanka <wiktoria.golanka@gmail.com>
2018-12-06 08:10:07 +0100

    I forgot that I should use different port for every socket connection, fixed

--- raspberrypi/FromBase.py
+++ raspberrypi/FromBase.py
@@ -2,27 +2,27 @@ import socket
 from raspberrypi.Mission import Mission
 
 HOST = '127.0.0.1'
-PORT = 62292
+PORT = 62295
 
 mission = Mission()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
-    socket_connection.bind((HOST, PORT))
-    socket_connection.listen()
-    connection, address = socket_connection.accept()
-    with connection:
-        print('Connected by', address)
-        while True:
-            data = connection.recv(1024)
+sock = socket.socket(socket.AF_INET,
+                     socket.SOCK_DGRAM)
 
-            command = data.decode()
+sock.bind((HOST, PORT))
 
-            if 'ABORT' in command:
-                print("Preparing to abort the mission")
-                Mission.operation_for(command)
-                command = 'no_command'
-                #TODO tutaj dodajemy wywołanie funkcji która zakończy
-                # misje i wyslę komunikat o jej zakończeniu
+while True:
+    data = sock.recv(1024)
 
-            # TODO tutaj kolejne ify dla reszty komend,
-            #  ale najpierw niech to wyżej zadziałą
+    command = data.decode()
+
+    if 'ABORT' in command:
+        print("Preparing to abort the mission")
+        Mission.operation_for(command)
+        command = 'no_command'
+
+        # TODO tutaj dodajemy wywołanie funkcji która zakończy
+        # misje i wyslę komunikat o jej zakończeniu
+
+    # TODO tutaj kolejne ify dla reszty komend,
+    #  ale najpierw niech to wyżej zadziałą
--- server/ToMegazord.py
+++ server/ToMegazord.py
@@ -1,21 +1,25 @@
 import socket
 
 HOST = '127.0.0.1'
-PORT = 62292
+PORT = 61456
+TO_PORT = 62295
 
 mission_abort = 1
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+sock = socket.socket(socket.AF_INET,
+                     socket.SOCK_DGRAM)
 
-    while True:
-        inputFromBase = input('Press 1 to abort mission\nPress 2 to continue')
-        command = 0
-        try:
-            command = int(inputFromBase)
-        except ValueError:
-            print('Invalid number!')
+sock.bind((HOST, PORT))
 
-        if command == 1:
-            s.send(str.encode('ABORT'))
-            print('\nSend command to abort mission!\n')
+while True:
+
+    inputFromBase = input('Press 1 to abort mission\nPress 2 to continue')
+    command = 0
+    try:
+        command = int(inputFromBase)
+    except ValueError:
+        print('Invalid number!')
+
+    if command == 1:
+        sock.sendto(str.encode('ABORT'), (HOST, TO_PORT))
+        print('\nSend command to abort mission!\n')
