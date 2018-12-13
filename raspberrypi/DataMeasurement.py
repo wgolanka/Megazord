46366b296e23d0b26824cac686c407eec41248c5
wgolanka <wiktoria.golanka@gmail.com>
2018-12-13 20:21:43 +0100

    ascasvavdas

---
+++ raspberrypi/DataMeasurement.py
@@ -0,0 +1,17 @@
+# from raspberrypi.MissionConfiguration import MissionConfig
+#
+# # class DataMeasurement:
+#
+# mission_config1 = MissionConfig()
+#
+# mission_config2 = MissionConfig()
+#
+# print("mission 1: ", mission_config1.LENGTH_IN_SECONDS)
+# print("mission 2: ", mission_config2.LENGTH_IN_SECONDS)
+# print("no mission num ", MissionConfig.LENGTH_IN_SECONDS)
+#
+# mission_config1.set_to(1444)
+#
+# print("mission 1: ", mission_config1.LENGTH_IN_SECONDS)
+# print("mission 2: ", mission_config2.LENGTH_IN_SECONDS)
+# print("no mission num ", MissionConfig.LENGTH_IN_SECONDS)
